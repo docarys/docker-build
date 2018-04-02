b0d4cf38f914f90e555a2abeb6334c566ba38fa2
Sergio Sisternes <sesispla@outlook.com>
2018-04-02 12:11:12 +0200

    tag latest

--- tag-latest.sh
+++ tag-latest.sh
@@ -1,2 +1,4 @@
-docker tag docarys:latest $DOCKER_USERNAME/docarys:latest-build
-docker push $DOCKER_USERNAME/docarys:latest-build
+if ["$LATEST" = true] ; then
+    docker tag docarys:latest $DOCKER_USERNAME/docarys:latest-build
+    docker push $DOCKER_USERNAME/docarys:latest-build
+fi;

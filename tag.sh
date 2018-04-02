f64c4b24cb0eb707cd21ac28d08f3e80a1dbdab3
Sergio Sisternes <sesispla@outlook.com>
2018-04-02 11:35:50 +0200

    Using again DOCKER_USERNAME

--- tag.sh
+++ tag.sh
@@ -1,5 +1,5 @@
-docker tag docarys:latest $DOCKER_REPO/docarys:$DOCARYS_SHORT_VERSION-$NODE_VERSION-build
-docker push $DOCKER_REPO/docarys:$DOCARYS_SHORT_VERSION-$NODE_VERSION-build
+docker tag docarys:latest $DOCKER_USERNAME/docarys:$DOCARYS_SHORT_VERSION-$NODE_VERSION-build
+docker push $DOCKER_USERNAME/docarys:$DOCARYS_SHORT_VERSION-$NODE_VERSION-build
 
-docker tag docarys:latest $DOCKER_REPO/docarys:$DOCARYS_VERSION-$NODE_VERSION-build
-docker push $DOCKER_REPO/docarys:$DOCARYS_VERSION-$NODE_VERSION-build
+docker tag docarys:latest $DOCKER_USERNAME/docarys:$DOCARYS_VERSION-$NODE_VERSION-build
+docker push $DOCKER_USERNAME/docarys:$DOCARYS_VERSION-$NODE_VERSION-build

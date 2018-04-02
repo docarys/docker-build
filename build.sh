971143e058197076404dd7a050c83506fcf1ada5
Sergio Sisternes <sesispla@outlook.com>
2018-04-02 11:53:46 +0200

    Updated travis.yml

--- build.sh
+++
@@ -1,4 +0,0 @@
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t docarys:latest . --build-arg NODE_VERSION=$NODE_VERSION
-docker images
--- tag.sh
+++
@@ -1,5 +0,0 @@
-docker tag docarys:latest $DOCKER_USERNAME/docarys:$DOCARYS_SHORT_VERSION-$NODE_VERSION-build
-docker push $DOCKER_USERNAME/docarys:$DOCARYS_SHORT_VERSION-$NODE_VERSION-build
-
-docker tag docarys:latest $DOCKER_USERNAME/docarys:$DOCARYS_VERSION-$NODE_VERSION-build
-docker push $DOCKER_USERNAME/docarys:$DOCARYS_VERSION-$NODE_VERSION-build

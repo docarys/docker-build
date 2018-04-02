8af368ee4871407df9140847d82e63bd21c54225
Sergio Sisternes <sesispla@outlook.com>
2018-04-02 11:13:26 +0200

    Added docarys short version to containers

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t docarys . --build-arg NODE_VERSION=$NODE_VERSION
+docker build -t docarys:latest . --build-arg NODE_VERSION=$NODE_VERSION
 docker images
--- tag.sh
+++ tag.sh
@@ -1,2 +1,5 @@
-docker tag docarys:latest $DOCKER_USERNAME/docarys:$NODE_VERSION-build
-docker push $DOCKER_USERNAME/docarys:$NODE_VERSION-build
+docker tag docarys:latest $DOCKER_USERNAME/docarys:$DOCARYS_SHORT_VERSION-$NODE_VERSION-build
+docker push $DOCKER_USERNAME/docarys:$DOCARYS_SHORT_VERSION-$NODE_VERSION-build
+
+docker tag docarys:latest $DOCKER_USERNAME/docarys:$DOCARYS_VERSION-$NODE_VERSION-build
+docker push $DOCKER_USERNAME/docarys:$DOCARYS_VERSION-$NODE_VERSION-build
